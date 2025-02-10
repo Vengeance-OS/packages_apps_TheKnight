Evolution-XYZ/packages_apps_Evolver

Evolver: A11 QS UI Style [3/3]
[neobuddy89: Remove contentobserver, not useful]

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/org/evolution/settings/fragments/quicksettings/QuickSettings.java
@@ -12,12 +12,15 @@ import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
 
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.evolution.ThemeUtils;
+
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
@@ -50,6 +53,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String KEY_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
     private static final String KEY_MISCELLANEOUS_CATEGORY = "quick_settings_miscellaneous_category";
     private static final String KEY_BLUETOOTH_AUTO_ON = "qs_bt_auto_on";
+    private static final String KEY_QS_UI_STYLE  = "qs_tile_ui_style";
 
     private static final int BATTERY_STYLE_PORTRAIT = 0;
     private static final int BATTERY_STYLE_TEXT = 4;
@@ -65,11 +69,16 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private SystemSettingSeekBarPreference mTileAnimationDuration;
     private SystemSettingListPreference mTileAnimationInterpolator;
     private PreferenceCategory mMiscellaneousCategory;
+    private ListPreference mQsUI;
+
+    private static ThemeUtils mThemeUtils;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.evolution_settings_quick_settings);
+
+        mThemeUtils = new ThemeUtils(getActivity());
 
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
@@ -117,6 +126,11 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         if (!DeviceUtils.deviceSupportsBluetooth(context)) {
             prefScreen.removePreference(mMiscellaneousCategory);
         }
+
+        mQsUI = (ListPreference) findPreference(KEY_QS_UI_STYLE);
+        mQsUI.setOnPreferenceChangeListener(this);
+
+        checkQSOverlays(context);
     }
 
     @Override
@@ -138,6 +152,13 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             int value = Integer.parseInt((String) newValue);
             updateTileAnimStyle(value);
             return true;
+        } else if (preference == mQsUI) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.QS_TILE_UI_STYLE, value, UserHandle.USER_CURRENT);
+            updateQsStyle(getActivity());
+            checkQSOverlays(getActivity());
+            return true;
         }
         return false;
     }
@@ -145,6 +166,45 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private void updateTileAnimStyle(int tileAnimationStyle) {
         mTileAnimationDuration.setEnabled(tileAnimationStyle != 0);
         mTileAnimationInterpolator.setEnabled(tileAnimationStyle != 0);
+    }
+
+    private static void updateQsStyle(Context context) {
+        ContentResolver resolver = context.getContentResolver();
+
+        boolean isA11Style = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_TILE_UI_STYLE , 0, UserHandle.USER_CURRENT) != 0;
+
+        String qsUIStyleCategory = "android.theme.customization.qs_ui";
+        String overlayThemeTarget  = "com.android.systemui";
+        String overlayThemePackage  = "com.android.system.qs.ui.A11";
+
+        if (mThemeUtils == null) {
+            mThemeUtils = new ThemeUtils(context);
+        }
+
+        // reset all overlays before applying
+        mThemeUtils.setOverlayEnabled(qsUIStyleCategory, overlayThemeTarget, overlayThemeTarget);
+
+        if (isA11Style) {
+            mThemeUtils.setOverlayEnabled(qsUIStyleCategory, overlayThemePackage, overlayThemeTarget);
+        }
+    }
+
+    private void checkQSOverlays(Context context) {
+        ContentResolver resolver = context.getContentResolver();
+        int isA11Style = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_TILE_UI_STYLE , 0, UserHandle.USER_CURRENT);
+
+        if (isA11Style > 0) {
+            mQsUI.setEnabled(true);
+        } else {
+            mQsUI.setEnabled(true);
+        }
+
+        // Update summaries
+        int index = mQsUI.findIndexOfValue(Integer.toString(isA11Style));
+        mQsUI.setValue(Integer.toString(isA11Style));
+        mQsUI.setSummary(mQsUI.getEntries()[index]);
     }
 
     @Override

Vengeance-OS/packages_apps_TheKnight

src/org/evolution/settings/fragments/quicksettings/QuickSettings.java
@@ -20,6 +20,8 @@ import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.evolution.ThemeUtils;
+
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
@@ -47,6 +49,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String KEY_INTERFACE_CATEGORY = "quick_settings_interface_category";
     private static final String KEY_MISCELLANEOUS_CATEGORY = "quick_settings_miscellaneous_category";
     private static final String KEY_QS_BLUETOOTH_SHOW_DIALOG = "qs_bt_show_dialog";
+    private static final String KEY_QS_UI_STYLE  = "qs_tile_ui_style";
     private static final String KEY_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
     private static final String KEY_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
 
@@ -58,15 +61,20 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private PreferenceCategory mMiscellaneousCategory;
     private ListPreference mShowBrightnessSlider;
     private ListPreference mBrightnessSliderPosition;
+    private ListPreference mQsUI;
     private LineageSecureSettingSwitchPreference mShowAutoBrightness;
     private SystemSettingListPreference mBatteryStyle;
     private SystemSettingListPreference mBatteryPercent;
     private SystemSettingSwitchPreference mBrightnessSliderHaptic;
 
+    private static ThemeUtils mThemeUtils;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.evolution_settings_quick_settings);
+
+        mThemeUtils = new ThemeUtils(getContext());
 
         final Context mContext = getContext();
         final ContentResolver resolver = mContext.getContentResolver();
@@ -109,6 +117,11 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         if (!DeviceUtils.deviceSupportsBluetooth(mContext)) {
             prefScreen.removePreference(mMiscellaneousCategory);
         }
+
+        mQsUI = (ListPreference) findPreference(KEY_QS_UI_STYLE);
+        mQsUI.setOnPreferenceChangeListener(this);
+
+        checkQSOverlays(mContext);
     }
 
     @Override
@@ -127,8 +140,48 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             mBatteryPercent.setEnabled(
                     value != BATTERY_STYLE_TEXT && value != BATTERY_STYLE_HIDDEN);
             return true;
+        } else if (preference == mQsUI) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.QS_TILE_UI_STYLE, value, UserHandle.USER_CURRENT);
+            updateQsStyle(getContext());
+            checkQSOverlays(getContext());
+            return true;
         }
         return false;
+    }
+
+    private static void updateQsStyle(Context context) {
+        ContentResolver resolver = context.getContentResolver();
+
+        boolean isA11Style = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_TILE_UI_STYLE , 0, UserHandle.USER_CURRENT) != 0;
+
+        String qsUIStyleCategory = "android.theme.customization.qs_ui";
+        String overlayThemeTarget  = "com.android.systemui";
+        String overlayThemePackage  = "com.android.system.qs.ui.A11";
+
+        if (mThemeUtils == null) {
+            mThemeUtils = new ThemeUtils(context);
+        }
+
+        // reset all overlays before applying
+        mThemeUtils.setOverlayEnabled(qsUIStyleCategory, overlayThemeTarget, overlayThemeTarget);
+
+        if (isA11Style) {
+            mThemeUtils.setOverlayEnabled(qsUIStyleCategory, overlayThemePackage, overlayThemeTarget);
+        }
+    }
+
+    private void checkQSOverlays(Context context) {
+        ContentResolver resolver = context.getContentResolver();
+        int isA11Style = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_TILE_UI_STYLE , 0, UserHandle.USER_CURRENT);
+
+        // Update summaries
+        int index = mQsUI.findIndexOfValue(Integer.toString(isA11Style));
+        mQsUI.setValue(Integer.toString(isA11Style));
+        mQsUI.setSummary(mQsUI.getEntries()[index]);
     }
 
     @Override