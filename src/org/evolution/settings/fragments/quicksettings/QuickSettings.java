Evolution-X/packages_apps_Evolver

Evolver: Add quick settings brightness slider customizations
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: AnierinB <anierin@evolution-x.org>

src/org/evolution/settings/fragments/quicksettings/QuickSettings.java
@@ -25,6 +25,10 @@ import com.android.settingslib.search.SearchIndexable;
 
 import java.util.List;
 
+import lineageos.preference.LineageSecureSettingListPreference;
+import lineageos.preference.LineageSecureSettingSwitchPreference;
+import lineageos.providers.LineageSettings;
+
 import org.evolution.settings.preferences.SystemSettingListPreference;
 
 @SearchIndexable
@@ -33,15 +37,23 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
     private static final String TAG = "QuickSettings";
 
+    private static final String KEY_INTERFACE_CATEGORY = "quick_settings_interface_category";
     private static final String KEY_BATTERY_STYLE = "qs_battery_style";
     private static final String KEY_BATTERY_PERCENT = "qs_show_battery_percent";
+    private static final String KEY_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
+    private static final String KEY_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
+    private static final String KEY_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
 
     private static final int BATTERY_STYLE_PORTRAIT = 0;
     private static final int BATTERY_STYLE_TEXT = 4;
     private static final int BATTERY_STYLE_HIDDEN = 5;
 
+    private PreferenceCategory mInterfaceCategory;
     private SystemSettingListPreference mBatteryStyle;
     private SystemSettingListPreference mBatteryPercent;
+    private LineageSecureSettingListPreference mShowBrightnessSlider;
+    private LineageSecureSettingListPreference mBrightnessSliderPosition;
+    private LineageSecureSettingSwitchPreference mShowAutoBrightness;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,6 +67,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
         mBatteryStyle = (SystemSettingListPreference) findPreference(KEY_BATTERY_STYLE);
         mBatteryPercent = (SystemSettingListPreference) findPreference(KEY_BATTERY_PERCENT);
+        mShowBrightnessSlider = (LineageSecureSettingListPreference) findPreference(KEY_SHOW_BRIGHTNESS_SLIDER);
+        mBrightnessSliderPosition = (LineageSecureSettingListPreference) findPreference(KEY_BRIGHTNESS_SLIDER_POSITION);
+        mShowAutoBrightness = (LineageSecureSettingSwitchPreference) findPreference(KEY_SHOW_AUTO_BRIGHTNESS);
 
         int batterystyle = Settings.System.getIntForUser(resolver,
                 Settings.System.QS_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
@@ -63,6 +78,20 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
         mBatteryPercent.setEnabled(
                 batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN);
+
+       mShowBrightnessSlider.setOnPreferenceChangeListener(this);
+        boolean showSlider = LineageSettings.Secure.getIntForUser(resolver,
+                LineageSettings.Secure.QS_SHOW_BRIGHTNESS_SLIDER, 1, UserHandle.USER_CURRENT) > 0;
+
+        mBrightnessSliderPosition.setEnabled(showSlider);
+
+        boolean autoBrightnessAvailable = resources.getBoolean(
+                com.android.internal.R.bool.config_automatic_brightness_available);
+        if (autoBrightnessAvailable) {
+            mShowAutoBrightness.setEnabled(showSlider);
+        } else {
+            mInterfaceCategory.removePreference(mShowAutoBrightness);
+        }
     }
 
     @Override
@@ -73,6 +102,12 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             int value = Integer.parseInt((String) newValue);
             mBatteryPercent.setEnabled(
                     value != BATTERY_STYLE_TEXT && value != BATTERY_STYLE_HIDDEN);
+            return true;
+        } else if (preference == mShowBrightnessSlider) {
+            int value = Integer.parseInt((String) newValue);
+            mBrightnessSliderPosition.setEnabled(value > 0);
+            if (mShowAutoBrightness != null)
+                mShowAutoBrightness.setEnabled(value > 0);
             return true;
         }
         return false;
@@ -90,6 +125,12 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             public List<String> getNonIndexableKeys(Context context) {
                 List<String> keys = super.getNonIndexableKeys(context);
                 final Resources resources = context.getResources();
+
+                boolean autoBrightnessAvailable = resources.getBoolean(
+                        com.android.internal.R.bool.config_automatic_brightness_available);
+                if (!autoBrightnessAvailable) {
+                    keys.add(KEY_SHOW_AUTO_BRIGHTNESS);
+                }
                 return keys;
             }
         };

Vengeance-OS/packages_apps_TheKnight

src/org/evolution/settings/fragments/quicksettings/QuickSettings.java
@@ -9,11 +9,14 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
 
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
@@ -23,6 +26,10 @@ import com.android.settingslib.search.SearchIndexable;
 
 import java.util.List;
 
+import lineageos.preference.LineageSecureSettingSwitchPreference;
+import lineageos.providers.LineageSettings;
+
+import org.evolution.settings.preferences.SystemSettingSwitchPreference;
 import org.evolution.settings.utils.DeviceUtils;
 
 @SearchIndexable
@@ -30,24 +37,55 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "QuickSettings";
+
+    private static final String KEY_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
+    private static final String KEY_BRIGHTNESS_SLIDER_HAPTIC = "qs_brightness_slider_haptic";
+    private static final String KEY_INTERFACE_CATEGORY = "quick_settings_interface_category";
     private static final String KEY_MISCELLANEOUS_CATEGORY = "quick_settings_miscellaneous_category";
     private static final String KEY_QS_BLUETOOTH_SHOW_DIALOG = "qs_bt_show_dialog";
+    private static final String KEY_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
+    private static final String KEY_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
 
+    private PreferenceCategory mInterfaceCategory;
     private PreferenceCategory mMiscellaneousCategory;
+    private ListPreference mShowBrightnessSlider;
+    private ListPreference mBrightnessSliderPosition;
+    private LineageSecureSettingSwitchPreference mShowAutoBrightness;
+    private SystemSettingSwitchPreference mBrightnessSliderHaptic;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.evolution_settings_quick_settings);
 
-        final Context context = getContext();
-        final ContentResolver resolver = context.getContentResolver();
+        final Context mContext = getContext();
+        final ContentResolver resolver = mContext.getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
-        final Resources resources = context.getResources();
+        final Resources resources = mContext.getResources();
+
+        mShowBrightnessSlider = findPreference(KEY_SHOW_BRIGHTNESS_SLIDER);
+        mShowBrightnessSlider.setOnPreferenceChangeListener(this);
+        boolean showSlider = LineageSettings.Secure.getIntForUser(resolver,
+                LineageSettings.Secure.QS_SHOW_BRIGHTNESS_SLIDER, 1, UserHandle.USER_CURRENT) > 0;
+
+        mBrightnessSliderPosition = findPreference(KEY_BRIGHTNESS_SLIDER_POSITION);
+        mBrightnessSliderPosition.setEnabled(showSlider);
+
+        mBrightnessSliderHaptic = findPreference(KEY_BRIGHTNESS_SLIDER_HAPTIC);
+        mBrightnessSliderHaptic.setEnabled(showSlider);
+
+        mShowAutoBrightness = findPreference(KEY_SHOW_AUTO_BRIGHTNESS);
+        boolean automaticAvailable = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_automatic_brightness_available);
+        if (automaticAvailable) {
+            mShowAutoBrightness.setEnabled(showSlider);
+        } else {
+            prefScreen.removePreference(mShowAutoBrightness);
+        }
 
         mMiscellaneousCategory = (PreferenceCategory) findPreference(KEY_MISCELLANEOUS_CATEGORY);
 
-        if (!DeviceUtils.deviceSupportsBluetooth(context)) {
+        if (!DeviceUtils.deviceSupportsBluetooth(mContext)) {
             prefScreen.removePreference(mMiscellaneousCategory);
         }
     }
@@ -56,6 +94,14 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
+        if (preference == mShowBrightnessSlider) {
+            int value = Integer.parseInt((String) newValue);
+            mBrightnessSliderPosition.setEnabled(value > 0);
+            mBrightnessSliderHaptic.setEnabled(value > 0);
+            if (mShowAutoBrightness != null)
+                mShowAutoBrightness.setEnabled(value > 0);
+            return true;
+        }
         return false;
     }
 
@@ -72,6 +118,11 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 List<String> keys = super.getNonIndexableKeys(context);
                 final Resources resources = context.getResources();
 
+                boolean autoBrightnessAvailable = resources.getBoolean(
+                        com.android.internal.R.bool.config_automatic_brightness_available);
+                if (!autoBrightnessAvailable) {
+                    keys.add(KEY_SHOW_AUTO_BRIGHTNESS);
+                }
                 if (!DeviceUtils.deviceSupportsBluetooth(context)) {
                     keys.add(KEY_QS_BLUETOOTH_SHOW_DIALOG);
                 }